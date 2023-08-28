9a753902398ac959cba91a8372e7bee5d8e20155
joyboy153 <140846088+joyboy153@users.noreply.github.com>
2023-08-28 17:08:20 +0300

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,75 +1,90 @@
-TweenMax.Form(".logo", 1, {
+document.addEventListener("DOMContentLoaded", function () {
+TweenMax.from(".logo", 1, {
     opacity: 0,
     x: -20,
-    ease:Expo.easeInOut
-})
-TweenMax.staggerForm("nav ul li", 1, {
+    ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom("nav ul li", 1, {
     opacity: 0,
     x: -20,
-    ease:Power3.easeInOut
-},0.08)
-TweenMax.Form(".btn_area", 1, {
+    ease: Power3.easeInOut
+}, 0.08);
+
+TweenMax.from(".btn_area", 1, {
     opacity: 0,
     y: -20,
-    delay: .9,
-    ease:Expo.easeInOut
-})
-TweenMax.staggerForm(".media ul li", 2, {
+    delay: 0.9,
+    ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom(".media ul li", 2, {
     opacity: 0,
-    delay:3.2,
+    delay: 3.2,
     y: 40,
-    ease:Expo.easeInOut
-},0.2)
-TweenMax.Form(".title_inner", 1, {
+    ease: Expo.easeInOut
+}, 0.2);
+
+TweenMax.from(".title_inner", 1, {
     opacity: 0,
     y: 20,
-    delay:1,
-    ease:Expo.easeInOut
-},0.2)
-TweenMax.Form(".description", 1, {
+    delay: 1,
+    ease: Expo.easeInOut
+}, 0.2);
+
+TweenMax.from(".description", 1, {
     opacity: 0,
     y: 20,
-    delay:1,
-    ease:Expo.easeInOut
-})
-TweenMax.Form(".box", 1, {
+    delay: 1,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".box", 1, {
     opacity: 0,
-    delay:1.5,
+    delay: 1.5,
     y: 20,
-    ease:Expo.easeInOut
-})
-TweenMax.Form(".banner_content .cnt_info .btn_area .btn", 1, {
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".banner_content .cnt_info .btn_area .btn", 1, {
     opacity: 0,
-    delay:1.4,
+    delay: 1.4,
     y: -20,
-    ease:Expo.easeInOut
-})
-TweenMax.Form(".follow_area", 2, {
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".follow_area", 2, {
     opacity: 0,
     y: 40,
-    ease:Expo.easeInOut
-},4.1)
-TweenMax.Form(".ecnt01", 1, {
+    ease: Expo.easeInOut
+}, 4.1);
+
+TweenMax.from(".ecnt01", 1, {
     opacity: 0,
-    delay:.4,
-    scale:0.1,
-    ease:Bounce.easeOut
-})
-TweenMax.Form(".ecnt02", 1, {
+    delay: 0.4,
+    scale: 0.1,
+    ease: Bounce.easeOut
+});
+
+TweenMax.from(".ecnt02", 1, {
     opacity: 0,
-    delay:.8,
-    scale:0.1,
-    ease:Back.easnOut
-})
-TweenMax.Form(".eimg02", 1, {
+    delay: 0.8,
+    scale: 0.1,
+    ease: Back.easeOut
+});
+
+TweenMax.from(".eimg02", 1, {
     opacity: 0,
-    delay:1,
-    scale:0.1,
-    ease:Bounce.easeOut
-})
-TweenMax.Form(".logo", 1, {
+    delay: 1,
+    scale: 0.1,
+    ease: Bounce.easeOut
+});
+
+TweenMax.from(".eimg01", 1, {
     opacity: 0,
-    delay:1.8,
-    scale:0.1,
-    ease:Bounce.easeOut
-})
+    delay: 1.8,
+    scale: 0.1,
+    ease: Bounce.easeOut
+});
+
+});
