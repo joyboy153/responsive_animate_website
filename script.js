b817cba2a74857a4c8e90c5a978426d87465726a
joyboy153 <140846088+joyboy153@users.noreply.github.com>
2023-08-28 17:07:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 var slides = document.querySelectorAll('.title_animation p');
 var slideindex = 0;
-slides[slideindex].classList.add(visible);
-setInterval (function () {
-    slides[slideindex].classList.remove(visible);
-    slides[slideindex].classList.add(hidden);
+slides[slideindex].classList.add('visible');
+setInterval(function () {
+    slides[slideindex].classList.remove('visible');
+    slides[slideindex].classList.add('hidden');
     slideindex = (slideindex + 1) % slides.length;
-    slides[slideindex].classList.remove(hidden);
-    slides[slideindex].classList.add(visible);
-}, 2000 );
+    slides[slideindex].classList.remove('hidden');
+    slides[slideindex].classList.add('visible');
+}, 2000);
 
-let mobilenav =document.querySelector('.mobile-menu');
-document.querySelector('.OC_btn').onclick = ()=>{
-    mobilenav.classList.add('active')
-}
-document.querySelector('closebtn').onclick = ()=>{
-    mobilenav.classList.remove('active');
-};;
+document.addEventListener('DOMContentLoaded', function() {
+    const mobilenav = document.querySelector('.mobile_menu');
+    
+    document.addEventListener('click', function(event) {
+        console.log('Clicked:', event.target);
+        if (event.target.matches('.OC_btn')) {
+            console.log('OC_btn clicked');
+            mobilenav.classList.add('active');
+        } else if (event.target.matches('.closebtn')) {
+            console.log('closebtn clicked');
+            mobilenav.classList.remove('active');
+        }
+    });
+});
 
-VanillaTilt.init(document.querySelectorAll(".box"), {
-    max: 15,
-    speed: 200
-}); 
+
+document.addEventListener('DOMContentLoaded', function() {
+    VanillaTilt.init(document.querySelectorAll('.box'), {
+        max: 15,
+        speed: 200
+    });
+});
+
